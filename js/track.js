6b160b15b1b59bd91cd984098878baf06592c520
pr!0ri7y <jai.priority@live.com>
2018-08-19 16:32:38 +0530

    commit
    commit

--- js/track.js
+++ js/track.js
@@ -5,11 +5,39 @@ $( document ).ready(function() {
         cache: false,
         async:false,       
         success: function(ip){
-          console.log(ip);
+          var details = getIpDetails(ip);
+          var res = postIpDetails(details);
         }
       });
 
-
-
-
 });
+
+
+function getIpDetails(ip) {
+
+        var request = new XMLHttpRequest();      
+        request.open('GET', 'http://ip-api.com/json/' + ip, false);
+        request.send(null);
+    
+        if (request.status === 200) {
+            return request.responseText;
+        }
+}
+
+function postIpDetails(details) {
+
+    // Url Construct
+    var Url = "https://balaleo.000webhostapp.com/tracker.php?" + "json=" + JSON.stringify(details);
+
+    var request = new XMLHttpRequest();
+    request.open('GET', Url, false);
+    request.send(null);
+
+    if (request.status === 200) {
+        console.log('tracked..');
+    }
+}
+
+
+
+
