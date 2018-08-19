9b22688ed9be1624c10803caa5bbbb3b0a55d3e3
pr!0ri7y <jai.priority@live.com>
2018-08-19 16:01:15 +0530

    commit
    commit

--- js/track.js
+++ js/track.js
@@ -4,6 +4,7 @@ $( document ).ready(function() {
         url: "https://balaleo.000webhostapp.com/ip.php",
         cache: false,
         async:false,
+        dataType : 'json',
         success: function(ip){
           console.log(JSON.parse(ip).ip);
         }
